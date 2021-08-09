8ae57c6f094283372e1c85f1b343f5f1db79b1a1
oleksandr.ushkarenko <maestrotees@gmail.com>
2021-08-09 21:45:56 +0300

    Code refactoring in task4.c (structure added).

--- task4.c
+++ task4.c
@@ -9,6 +9,7 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <inttypes.h>
 
 /*
  * The number of inches in 1 meter.
@@ -16,12 +17,31 @@
 #define INCHES_IN_METER 39.3701
 
 /*
+ * The codes that describe the results of cheking rectangle parameters
+ * received from the user.
+ */
+#define VALID 0
+#define INVALID -1
+
+/*
+ * The data structure is used to store the information about rectangle,
+ * such as height and width.
+ */
+typedef struct {
+        double height;
+        double width;
+} RECT;
+
+/*
  * Function prototypes.
  */
+RECT build_rect(void);
 double get_rect_height(void);
 double get_rect_width(void);
-double rect_perimeter_inches(const double height, const double width);
-double rect_area_inches(const double height, const double width);
+double calc_rect_perimeter_inches(const RECT rect);
+double calc_rect_area_inches(const RECT rect);
+void print_rect_params(double perimeter, double area);
+int validate_rect(RECT rect);
 
 /*
  * The main function of the program.
@@ -34,25 +54,33 @@ double rect_area_inches(const double height, const double width);
  */
 int main(int argc, char *argv[])
 {
-        double rect_height = 0;
-        double rect_width = 0;
+        RECT rect;
         double perimeter = 0;
         double area = 0;
-        
-        rect_height = get_rect_height();
-        rect_width = get_rect_width();
 
-        if((rect_height >= 0) && (rect_width >= 0)) {
-                perimeter = rect_perimeter_inches(rect_height, rect_width);
-                area = rect_area_inches(rect_height, rect_width);
-                
-                printf("The perimeter of the rectangle is %.2lf inches.\n", perimeter);
-                printf("The area of the rectangle is %.2lf square inches.\n", area);
-        } else {
-                printf("Height and width of a rectangle must be positive numbers!\n");
-        }
+        rect = build_rect();    
+
+        if(validate_rect(rect) == VALID) {
+                perimeter = calc_rect_perimeter_inches(rect);
+                area = calc_rect_area_inches(rect);
+                print_rect_params(perimeter, area);
+        } 
 
         return 0;
+}
+
+/*
+ * The function creates the instance of RECT structure and fills its fields by
+ * receiving all the necessary values from the user.
+ * 
+ * @return the instance of RECT data structure
+ */
+RECT build_rect(void)
+{
+        RECT rect;
+        rect.height = get_rect_height();
+        rect.width = get_rect_width();
+        return rect;
 }
 
 /*
@@ -90,23 +118,52 @@ double get_rect_width(void)
 /*
  * The function calculates the perimeter of the rectangle in inches.
  *
- * @param height the height of the rectangle in meters
- * @param width the width of the rectngle in meters
+ * @param the instance of RECT data structure
  * @return the perimeter of the the rectangle in inches
  */
-double rect_perimeter_inches(const double height, const double width)
+double calc_rect_perimeter_inches(const RECT rect)
 {
-        return 2 * (height + width) * INCHES_IN_METER;
+        return 2 * (rect.height + rect.width) * INCHES_IN_METER;
 }
 
 /*
  * The function calculates the area of the rectangle in square inches.
  *
- * @param height the height of the rectangle in meters
- * @param width the width of the rectngle in meters
+ * @param rect the instance of RECT data structure
  * @return the area of the the rectangle in inches
  */
-double rect_area_inches(const double height, const double width)
+double calc_rect_area_inches(const RECT rect)
 {
-        return height * width * pow(INCHES_IN_METER, 2);
+        return rect.height * rect.width * pow(INCHES_IN_METER, 2);
+}
+
+/*
+ * The function prints the perimeter and the area of the rectangle in square inches.
+ *
+ * @param perimeter the perimeter of the rectangle
+ * @param area the area of the rectangle
+ */
+void print_rect_params(double perimeter, double area)
+{
+        printf("The perimeter of the rectangle is %.2lf inches.\n", perimeter);
+        printf("The area of the rectangle is %.2lf square inches.\n", area);
+}
+
+/*
+ * The function checks if the height and the width of the rectangle
+ * are not negative. If one af the fields of the structure is negative,
+ * the information message for user will be printed.
+ *
+ * @param rect the instance of RECT data structure to be checked
+ * @return VALID if height and width of the rectangle are not negative, 
+ * otherwise return IVNALID 
+ */
+int validate_rect(RECT rect)
+{
+  if((rect.height >= 0) && (rect.width >= 0)) {
+          return VALID;
+  } else {
+        printf("Height and width of a rectangle must be positive numbers!\n");
+  }
+  return INVALID;
 }
